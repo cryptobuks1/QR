09a37a238712094bc67d770041b28ffb378e04dc
Rahul kumar Bharti <rkb6280@gmail.com>
2020-05-12 10:28:05 +0530

    done

--- QR_based/settings.py
+++ QR_based/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+uw57h5pq3i%!@bbeh2+-obmva&c&=*svv32qyfh3i6u5yr7=%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- database/mail.py
+++ database/mail.py
@@ -1,5 +1,10 @@
-import smtplib
 import email.message
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # sender = 'python.vmm@gmail.com'
 # password = 'vmmpython'
@@ -44,22 +49,7 @@ def checkthis():
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
                 </head>
                 <body>
-                    <table  width="100%" cellpadding="0" cellspacing="0" bgcolor="e4e4e4"">
-                    <tr>
-                        <td></td>
-                        <td> {username}</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Tap the button below to log in to your Zomato account</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><a  href="127.0.0.1/opt_login?user={receiver}">Log in Zomato</a></td>
-                        <td></td>
-                    </tr>
+                    
                 </table>
             </body>
                 """
@@ -75,4 +65,70 @@ def checkthis():
     print('Sent')
     smtpserver.close()
 
-checkthis()
+
+def mailwithAttachemnt(reciver,id,filelocation):
+
+    subject = "QR Code"
+    # body = "This is an email with attachment sent from Python"
+
+    body = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <p>dear member,<br>
+        your id number is {}. download this file</p>
+    </body>
+    </html>
+    """.format(id)
+
+    sender_email = "tania.vmmteachers23@gmail.com"
+    receiver_email = reciver
+    password = "Teachers@123"
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+
+    # Add body to email
+    # message.attach(MIMEText(body, "plain"))
+    message.attach(MIMEText(body, "html"))
+
+    # filename = "../static/pdf/8SigneRichardson.pdf"  # In same directory as script
+    filename = filelocation  # In same directory as script
+
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
+
+if __name__ == '__main__':
+    mailwithAttachemnt('rkb9874@gmail.com',2,"../static/pdf/9RanaMccormick.pdf")
